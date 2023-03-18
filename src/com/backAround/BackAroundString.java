3edfc730f5f43802f13507349d39ddae82487cfd
biplab-w <waglebip@gmail.com>
2023-03-18 17:20:27 -0500

    adds last string character to front and back
    gets a string and adds the last character of the string to the front and back of the string

---
+++ src/com/backAround/BackAroundString.java
@@ -0,0 +1,28 @@
+package com.backAround;
+
+public class BackAroundString {
+	public static void main(String[] args) {
+		
+//Given a string, take the last char and return a new string 
+//with the last char added at the front and back, so "cat" yields "tcatt". 
+//The original string will be length 1 or more.
+//
+//
+//				backAround("cat") → "tcatt"
+//				backAround("Hello") → "oHelloo"
+//				backAround("a") → "aaa"
+		BackAroundString ba = new BackAroundString();
+		System.out.println(ba.backAround("cat"));
+		System.out.println(ba.backAround("Hello"));
+		System.out.println(ba.backAround("a"));
+		
+		
+	}
+	
+	public String backAround(String str) {
+		 //Get the last character
+		String last = str.substring(str.length()-1);
+		//Add and return last character + the string itself + the last character
+		return last + str + last;
+	}
+}
